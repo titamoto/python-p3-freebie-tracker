db9495d9bac9445388f4d729202b9ff7d6e55ba4
Aleksandr <titamoto@gmail.com>
2023-08-07 11:29:03 -0700

    create db, all three tables

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy 
 
 convention = {
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -16,6 +17,11 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    freebies = relationship('Freebie', backref=backref('company'))
+
+    devs = association_proxy('freebies', 'dev',
+           creator=lambda dev: Freebie(dev=dev))
+
     def __repr__(self):
         return f'<Company {self.name}>'
 
@@ -23,7 +29,24 @@ class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
+
+    freebies = relationship('Freebie', backref=backref('dev'))
+
+    companies = association_proxy('freebies', 'company',
+           creator=lambda company: Freebie(company=company))
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+
+    def __repr__(self):
+        return f'<Freebie {self.item_name}>'
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,39 @@
 #!/usr/bin/env python3
 
-# Script goes here!
+from random import choice as rc
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from models import Dev, Company, Freebie
+
+engine = create_engine('sqlite:///seed_db.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def delete_records():
+    session.query(Dev).delete()
+    session.query(Company).delete()
+    session.query(Freebie).delete()
+    session.commit()
+
+def create_records():
+    devs = [Dev() for i in range(500)]
+    companies = [Company() for i in range(100)]
+    freebies = [Freebie() for i in range(1000)]
+    session.add_all(devs + companies + freebies)
+    session.commit()
+    return devs, companies, freebies
+
+def relate_records(devs, companies, freebies):
+    for freebie in freebies:
+        freebie.dev = rc(devs)
+        freebie.company = rc(companies)
+
+    session.add_all(freebies)
+    session.commit()
+
+if __name__ == '__main__':
+    delete_records()
+    devs, companies, freebies = create_records()
+    relate_records(devs, companies, freebies)
